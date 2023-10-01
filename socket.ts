29556d25e6339d31c039606fbb722d39acfb9452
rn-mgl <reinb2ra@gmail.com>
2023-10-01 18:37:59 +0800

    fix url

--- socket.ts
+++ socket.ts
@@ -3,4 +3,4 @@ import { io } from "socket.io-client";
 const local = "http://192.168.1.121:9000";
 const prod = "https://synchroflow-server.onrender.com";
 
-export const socket = io(local, { autoConnect: false });
+export const socket = io(prod, { autoConnect: false });
