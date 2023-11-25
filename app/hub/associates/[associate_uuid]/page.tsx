0d4faf87f5191a2960679a88fd6ff23c453e67ff
rn-mgl <reinb2ra@gmail.com>
2023-11-25 08:57:33 +0800

    can see the profiles of collaborators in cards

---
+++ app/hub/associates/[associate_uuid]/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SingleAssociate = () => {
+  return <div>SingleAssociate</div>;
+};
+
+export default SingleAssociate;
--- src/components/tasks/TaskCards.tsx
+++ src/components/tasks/TaskCards.tsx
@@ -2,6 +2,9 @@ import Link from "next/link";
 import React from "react";
 import { AiOutlineClockCircle } from "react-icons/ai";
 import { localizeDate } from "../utils/dateUtils";
+import { useGlobalContext } from "@/base/context";
+import { useSession } from "next-auth/react";
+import axios from "axios";
 
 interface TaskCardProps {
   title: string;
@@ -12,20 +15,62 @@ interface TaskCardProps {
   deadline: string;
 }
 
-const profilePictures = new Array(20).fill(1);
-const profilePicsLen = profilePictures.length;
+interface CollaboratorsProps {
+  name: string;
+  surname: string;
+  user_uuid: string;
+  image: string;
+}
 
 const TaskCards: React.FC<TaskCardProps> = (props) => {
-  const mappedProfilePictures = profilePictures.slice(0, profilePicsLen < 5 ? profilePicsLen : 5).map((pics, index) => {
-    return (
-      <div
-        key={index}
-        style={{ right: `${index}rem` }}
-        className="w-6 h-6 rounded-full bg-secondary-300 
-                border-2 border-white absolute"
-      />
-    );
-  });
+  const [collaborators, setCollaborators] = React.useState<Array<CollaboratorsProps>>([]);
+
+  const { url } = useGlobalContext();
+  const { data: session } = useSession();
+  const user = session?.user;
+
+  const getCollaborators = React.useCallback(async () => {
+    if (user?.token) {
+      try {
+        const { data } = await axios.get(`${url}/main_task_collaborators`, {
+          headers: { Authorization: user?.token },
+          params: { mainTaskUUID: props.taskUUID },
+        });
+        if (data) {
+          setCollaborators(data);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  }, [url, user?.token, props.taskUUID]);
+
+  const mappedCollaborators = collaborators
+    .slice(0, collaborators.length < 5 ? collaborators.length : 5)
+    .map((collaborator, index) => {
+      return (
+        <Link
+          href={`/hub/associates/${collaborator.user_uuid}`}
+          style={{ right: `${index}rem`, backgroundImage: `url(${collaborator.image})` }}
+          key={index}
+          className="w-6 h-6 rounded-full bg-secondary-300 
+          border-2 border-white absolute bg-cover bg-center group"
+        >
+          <div
+            className="hidden group-hover:flex absolute -translate-y-7 
+                        left-2/4 -translate-x-2/4 bg-secondary-500 text-white rounded-sm p-1"
+          >
+            <p className="text-xs whitespace-nowrap">
+              {collaborator.name} {collaborator.surname}
+            </p>
+          </div>
+        </Link>
+      );
+    });
+
+  React.useEffect(() => {
+    getCollaborators();
+  }, [getCollaborators]);
 
   return (
     <div className="flex flex-row gap-4 justify-center min-w-[20rem] w-80 h-full select-none">
@@ -33,7 +78,8 @@ const TaskCards: React.FC<TaskCardProps> = (props) => {
         <Link
           href={`/hub/tasks/${props.taskUUID}`}
           style={{ backgroundImage: `url(${props.banner})` }}
-          className="bg-primary-100 w-full h-full rounded-2xl bg-center bg-cover"
+          className="bg-primary-100 w-full h-full rounded-2xl bg-center bg-cover
+                      hover:shadow-[0rem_0.2rem_0.4rem_#141522_inset] transition-all"
         />
 
         <div className="w-full flex flex-row justify-between">
@@ -42,7 +88,7 @@ const TaskCards: React.FC<TaskCardProps> = (props) => {
         </div>
 
         <div className="flex flex-col w-full gap-1">
-          <div className="flex justify-between">
+          <div className="flex justify-between text-sm">
             <p className="font-bold text-secondary-400">Progress</p>
             <p className="text-primary-500 capitalize">{props.status}</p>
           </div>
@@ -53,10 +99,12 @@ const TaskCards: React.FC<TaskCardProps> = (props) => {
             <div>
               <AiOutlineClockCircle className="text-xl" />
             </div>
-            <p>{localizeDate(props.deadline, false)}</p>
+            <p className="text-sm">{localizeDate(props.deadline, false)}</p>
           </div>
 
-          <div className="flex flex-row gap-2 items-center justify-center relative">{mappedProfilePictures}</div>
+          <div className="flex flex-row gap-2 items-center justify-center relative">
+            {collaborators.length ? mappedCollaborators : <p>-</p>}
+          </div>
         </div>
       </div>
     </div>
