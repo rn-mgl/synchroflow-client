a28ce4fe7811edd8f2bbb515b3e5eac67e7e85b0
rn-mgl <reinb2ra@gmail.com>
2023-12-10 20:34:34 +0800

    can fetch sent and received invites for tasks and associates

--- app/hub/invites/page.tsx
+++ app/hub/invites/page.tsx
@@ -1,7 +1,176 @@
+"use client";
+
+import { useGlobalContext } from "@/base/context";
+import axios from "axios";
+import { useSession } from "next-auth/react";
 import React from "react";
 
 const Invites = () => {
-  return <div>Invites</div>;
+  const [sentTaskInvites, setSentTaskInvites] = React.useState([]);
+  const [receivedTaskInvites, setReceivedTaskInvites] = React.useState([]);
+  const [sentAssociateInvites, setSentAssociateInvites] = React.useState([]);
+  const [receivedAssociateInvites, setReceivedAssociateInvites] = React.useState([]);
+
+  const { url } = useGlobalContext();
+  const { data: session } = useSession();
+  const user = session?.user;
+
+  const getSentTaskInvites = React.useCallback(async () => {
+    if (user?.token) {
+      try {
+        const { data } = await axios.get(`${url}/main_task_invites`, {
+          headers: { Authorization: user?.token },
+          params: { type: "sent" },
+        });
+        if (data) {
+          setSentTaskInvites(data);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  }, [url, user?.token]);
+
+  const getReceivedTaskInvites = React.useCallback(async () => {
+    if (user?.token) {
+      try {
+        const { data } = await axios.get(`${url}/main_task_invites`, {
+          headers: { Authorization: user?.token },
+          params: { type: "received" },
+        });
+        if (data) {
+          setReceivedTaskInvites(data);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  }, [url, user?.token]);
+
+  const getSentAssociateInvites = React.useCallback(async () => {
+    if (user?.token) {
+      try {
+        const { data } = await axios.get(`${url}/associate_invites`, {
+          headers: { Authorization: user?.token },
+          params: { type: "sent" },
+        });
+        if (data) {
+          setSentAssociateInvites(data);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  }, [url, user?.token]);
+
+  const getReceivedAssociateInvites = React.useCallback(async () => {
+    if (user?.token) {
+      try {
+        const { data } = await axios.get(`${url}/associate_invites`, {
+          headers: { Authorization: user?.token },
+          params: { type: "received" },
+        });
+        if (data) {
+          setReceivedAssociateInvites(data);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  }, [url, user?.token]);
+
+  React.useEffect(() => {
+    getSentTaskInvites();
+  }, [getSentTaskInvites]);
+
+  React.useEffect(() => {
+    getReceivedTaskInvites();
+  }, [getReceivedTaskInvites]);
+
+  React.useEffect(() => {
+    getSentAssociateInvites();
+  }, [getSentAssociateInvites]);
+
+  React.useEffect(() => {
+    getReceivedAssociateInvites();
+  }, [getReceivedAssociateInvites]);
+
+  return (
+    <div className="flex flex-col items-center justify-start w-full h-auto">
+      <div
+        className="max-w-screen-2xl flex flex-col justify-start 
+            items-center w-full h-full"
+      >
+        <div className="flex flex-col w-full items-center justify-start p-4 t:p-10 gap-4 h-auto">
+          <div className="w-full flex flex-col gap-2 rounded-lg items-center h-44">
+            <div className="flex flex-row justify-between w-full">
+              <p className="font-semibold">Sent Task Invites</p>
+            </div>
+
+            <div className="relative flex flex-row gap-4 w-full h-full overflow-x-hidden items-center justify-start">
+              <div
+                className="absolute w-full h-full flex flex-row gap-4 items-center justify-start 
+                  transition-all task-scroller p-2 overflow-x-auto cstm-scrollbar"
+              >
+                {/* {mappedRecentAssociateCards} */}
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="flex flex-col w-full items-center justify-start p-4 t:p-10 gap-4 h-auto">
+          <div className="w-full flex flex-col gap-2 rounded-lg items-center h-44">
+            <div className="flex flex-row justify-between w-full">
+              <p className="font-semibold">Received Task Invites</p>
+            </div>
+
+            <div className="relative flex flex-row gap-4 w-full h-full overflow-x-hidden items-center justify-start">
+              <div
+                className="absolute w-full h-full flex flex-row gap-4 items-center justify-start 
+                  transition-all task-scroller p-2 overflow-x-auto cstm-scrollbar"
+              >
+                {/* {mappedRecentAssociateCards} */}
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="flex flex-col w-full items-center justify-start p-4 t:p-10 gap-4 h-auto">
+          <div className="w-full flex flex-col gap-2 rounded-lg items-center h-44">
+            <div className="flex flex-row justify-between w-full">
+              <p className="font-semibold">Sent Associate Invites</p>
+            </div>
+
+            <div className="relative flex flex-row gap-4 w-full h-full overflow-x-hidden items-center justify-start">
+              <div
+                className="absolute w-full h-full flex flex-row gap-4 items-center justify-start 
+                  transition-all task-scroller p-2 overflow-x-auto cstm-scrollbar"
+              >
+                {/* {mappedRecentAssociateCards} */}
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="flex flex-col w-full items-center justify-start p-4 t:p-10 gap-4 h-auto">
+          <div className="w-full flex flex-col gap-2 rounded-lg items-center h-44">
+            <div className="flex flex-row justify-between w-full">
+              <p className="font-semibold">Received Associate Invites</p>
+            </div>
+
+            <div className="relative flex flex-row gap-4 w-full h-full overflow-x-hidden items-center justify-start">
+              <div
+                className="absolute w-full h-full flex flex-row gap-4 items-center justify-start 
+                  transition-all task-scroller p-2 overflow-x-auto cstm-scrollbar"
+              >
+                {/* {mappedRecentAssociateCards} */}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Invites;
