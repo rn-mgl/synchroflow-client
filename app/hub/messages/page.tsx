2afea4ffc18a267ea500a201e652f0bd690cb0d0
rn-mgl <reinb2ra@gmail.com>
2024-01-12 19:57:26 +0800

    real time add and remove member

--- app/hub/messages/page.tsx
+++ app/hub/messages/page.tsx
@@ -174,6 +174,13 @@ const Messages = () => {
   }, [socket, searchFilter, getMessageRooms]);
 
   React.useEffect(() => {
+    socket.on("reflect_remove_group_member", async () => {
+      await getMessageRooms(searchFilter);
+      handleSelectedMessageRoom("", "back");
+    });
+  }, [socket, searchFilter, selectedMessageRoom, getMessageRooms, handleSelectedMessageRoom]);
+
+  React.useEffect(() => {
     socket.on("get_messages", async (args: { room: string }) => {
       await getMessageRoomMessages();
       if (args.room !== user?.uuid) {
