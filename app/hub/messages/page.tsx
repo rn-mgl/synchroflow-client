6297c7f6f581ebe412796f49a90d3a0a0a1f3988
rn-mgl <reinb2ra@gmail.com>
2026-01-31 10:30:02 +0800

    fixes

--- app/hub/messages/page.tsx
+++ app/hub/messages/page.tsx
@@ -137,7 +137,7 @@ const Messages = () => {
       const { data } = await axios.delete(
         `${url}/message_rooms/${activeRoom.message_room}`,
         {
-          headers: { Authorization: user, roomType },
+          headers: { Authorization: user?.token, roomType },
         },
       );
       if (data.deletedRoom) {
@@ -595,7 +595,6 @@ const Messages = () => {
                 <div className="flex flex-row w-full gap-4">
                   <div className="flex flex-row items-center justify-center rounded-md w-full gap-4 bg-neutral-100 p-2">
                     <div
-                      placeholder="Aa"
                       onKeyDown={(e) => handleMessagePanelKeys(e)}
                       contentEditable={true}
                       ref={messageRef as RefObject<HTMLDivElement>}
